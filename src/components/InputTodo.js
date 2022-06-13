1c4461038cbb47357202d42d90f62a268e4717d3
Abass <Abassolanrewaju111@gmail.com>
2022-06-13 19:57:30 +0100

    add onChange method to cotrol input value

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 class InputTodo extends Component {
-    state = {
-        title: ''
-    }
-    render() {
-        return (
-            <form>
-                <input type= 'text' placeholder="Add Todo....." />
-                <button>Submit</button>
-            </form>
-        )
-    }
+  state = {
+    title: "",
+  };
+
+  onChange = event => {
+    this.setState ({
+        [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add Todo....."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
 export default InputTodo;
