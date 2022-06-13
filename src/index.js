8a110b1a4aeb705b68fe8042833613ac99bdf4b5
Abass <Abassolanrewaju111@gmail.com>
2022-06-13 11:35:10 +0100

    Render ToDoContainer to ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import ToDoContainer from './components/ToDoContainer.js'
 
-const element = <h1>Hello from Create React App</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(<ToDoContainer />, document.getElementById("root"))
