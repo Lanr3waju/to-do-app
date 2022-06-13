c33ec83f11bfbc3520cd2e09d5b53362b2ffece5
Abass <Abassolanrewaju111@gmail.com>
2022-06-13 12:31:52 +0100

    convert TodoItem to a function component

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ToDoItem from "./TodoItem";
 class ToDoList extends React.Component {
     render() {
         return (
             <ul>
             {this.props.todos.map((todo) => (
-              <li key={todo.id}>{todo.title}</li>
+                <ToDoItem key={todo.id} todo={todo} />
             ))}
           </ul>   
         )
---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoItem = props => {
+        return <li>{props.todo.title}</li>   
+    }
+
+export default ToDoItem;
